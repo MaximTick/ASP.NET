6e65f02cbfbe2e174d0e64e56480911022609ba7
Maxim <maxim.tickhonovich@gmail.com>
2019-03-22 21:44:49 +0300

    lab5_task3

---
+++ Lab05/Lab05/Form3.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Form3.aspx.cs" Inherits="Lab05.Form3" %>
+<%@ OutputCache Duration="10" VaryByParam="param1; param2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>task3</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Now: "></asp:Label>
+            <asp:Button ID="Button1" runat="server" Text="Кнопка" OnClick="Button1_Click" Width="70px" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Lab05/Lab05/Form3.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lab05
+{
+    public partial class Form3 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            String x = "";
+            foreach (String s in Request.QueryString)
+            {
+                x += (" " + s + " " + Request[s]);
+            }
+            Label1.Text += (x + ": " + DateTime.Now.ToString() + "<br/>");
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
